9f94ea90c74669924cab9bf29c059e7645100e19
abhaykharat-bit <256808622+abhay-kharat@users.noreply.github.com>
2026-08-23 17:29:42 +0530

    chore: remove legacy static portfolio data

--- src/data/portfolioData.js
+++
@@ -1,9 +0,0 @@
-// Re-export portfolio dataset matching exact resume information for Abhay Kharat
-export {
-  PROFILE_DATA,
-  INITIAL_PROJECTS,
-  SKILL_CATEGORIES,
-  INITIAL_EXPERIENCE,
-  INITIAL_EDUCATION,
-  INITIAL_CERTIFICATIONS
-} from '../lib/data';
